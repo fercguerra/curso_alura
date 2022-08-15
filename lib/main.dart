f1fb227857f44dadb50be3b82b2ae88f14aca5c9
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-08-14 22:18:00 -0300

    Text

--- lib/main.dart
+++ lib/main.dart
@@ -16,62 +16,78 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.red,
-                height: 100,
-                width: 100,
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.blue,
+                  height: 100,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: const Text(
+                'Diamante amarelo',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
               ),
-              Container(
-                color: Colors.blue,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                color: Colors.blue,
-                height: 100,
-                width: 100,
-              ),
-              Container(
-                color: Colors.red,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.cyan,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.pinkAccent,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.purple,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
