74d614a40937e5c8e76850ad2cd7df151def27a2
Fernanda Cristina Guerra <60983422+fercguerra@users.noreply.github.com>
2022-08-14 21:23:25 -0300

    slack e container

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(color: Colors.red,
+          width: 100,
+          height: 100,
+          ),
+          Container(
+            color: Colors.blue,
+            width: 50,
+            height: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
